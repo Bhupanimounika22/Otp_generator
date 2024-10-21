81baf635d32c84219b2b1152c6993e6f51aea878
Mounika <127584838+Bhupanimounika22@users.noreply.github.com>
2024-10-21 18:58:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+const otpGenerator = require('otp-generator');
+
+// Log to verify environment variables
+console.log('Email ID:', process.env.EMAIL_ID);
+console.log('Email Password:', process.env.EMAIL_PASSWORD);
+
+// Generate OTP
+const otp = otpGenerator.generate(6, { upperCaseAlphabets: false, specialChars: false });
+
+// Configure nodemailer
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_ID,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+// Email options
+const mailOptions = {
+  from: process.env.EMAIL_ÌD,
+  to: 'laxmisai.21mis7119@vitapstudent.ac.in',
+  subject: 'Your OTP Code',
+  text: `Your OTP code is ${otp}`,
+};
+
+// Send email
+transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    console.log('Error occurred while sending OTP:', error);
+  } else {
+    console.log('OTP sent successfully:', info.response);
+    console.log('OTP:', otp);
+  }
+});
